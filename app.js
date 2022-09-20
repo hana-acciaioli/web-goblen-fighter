25b3a2919bb36fa449a476f747a7477c8d2d3abb
Hana Acciaioli <hana.acciaioli@gmail.com>
2022-09-20 11:30:38 -0700

    Display of user (HP, alive/dead)

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 /* Imports */
 
 /* Get DOM Elements */
+// const scoreboard = document.getElementById('scoreboard');
+// const message = document.getElementById('message');
+
+const playerHP = document.getElementById('player-hp');
+const playerImage = document.getElementById('player-image');
 
 /* State */
+let player = {
+    hp: 10,
+    type: 'hero',
+};
 
 /* Events */
 
 /* Display Functions */
-
+function displayPlayer() {
+    playerHP.textContent = Math.max(0, player.hp);
+    if (player.hp < 1) {
+        playerImage.src = 'assets/headstone.png';
+    } else if (player.hp < 5) {
+        playerImage.src = 'assets/jack-o-lantern-sad.png';
+    } else {
+        playerImage.src = 'assets/jack-o-lantern.png';
+    }
+}
 // (don't forget to call any display functions you want to run on page load!)
+displayPlayer();
